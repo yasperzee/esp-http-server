a904587f0fd34e0074d188a81d3535367815cdf8
yasperzee <yasperzee@gmail.com>
2022-11-09 02:45:43 +0200

    cleaning a bit

--- include/read_dht_sensor.h
+++ include/read_dht_sensor.h
@@ -15,24 +15,7 @@
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
 
-<<<<<<< HEAD
-// values from DHT read_dht_sensor
-struct Values{
-    float temperature;
-    float humidity;
-    };
 
-    // Select DHT sensor in use, select "SENSOR_STR" also !
-    //#define DHT_TYPE      DHT11
-    //String SENSOR_STR =   "DHT-11";
-    #define DHT_TYPE 	  DHT22
-    String SENSOR_STR = "DHT-22";
-    #define DHT_PIN 	2 // ESP-01 gpio 2
-
-    // constants
-    const float ErrorValue = -999.9;
-
-=======
  #define DHT_PIN 2 // ESP01 and ESP12 uses gpio 2 for DHT11 / DHT22 sensor
  #define DHT_TYPE DHT11
     //#define SENSOR_STR "DHT22"
@@ -48,6 +31,5 @@ struct Values{
    };
 
 Values read_dht_sensor(void);
->>>>>>> Last-Arduino-IDE-version
 
 #endif // READ_DHT_SENSOR_H
